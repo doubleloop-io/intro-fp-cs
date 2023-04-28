c347ba35c531639a7155d17c6ed8d7673482086e
Matteo Baglini <matteo.baglini@gmail.com>
2023-04-28 16:49:55 +0200

    add list exercises

---
+++ IntroFp/ListTests.cs
@@ -0,0 +1,119 @@
+using LanguageExt;
+using Xunit;
+
+namespace IntroFp;
+
+public class ListTests
+{
+    [Fact(Skip = "skip")]
+    public void linq_filled_list()
+    {
+        // create a list of ints
+        var result = new List<int> { 12, 9, 555 }
+            // convert every value into a string
+            // keep the list with the same size
+            .Select(n => n.ToString())
+            // duplicate every value
+            // keep the list but change the size
+            .SelectMany(s => new List<string> { s, s })
+            // remove the list with a cumulative result
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+
+    [Fact(Skip = "skip")]
+    public void linq_empty_list()
+    {
+        // create an empty list of ints
+        var result = new List<int>()
+            // convert every value into a string
+            // keep the list with the same size
+            .Select(n => n.ToString())
+            // duplicate every value
+            // keep the list but change the size
+            .SelectMany(s => new List<string> { s, s })
+            // remove the list with a cumulative result
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+
+    [Fact(Skip = "skip")]
+    public void not_linq_but_functional_programming()
+    {
+        // create a list of ints
+        var result = new List<int> { 12, 9, 555 }
+            // convert every value into a string (LangExt)
+            // keep the list with the same size
+            .Map(n => n.ToString())
+            // duplicate every value (LangExt)
+            // keep the list but change the size
+            .Bind(s => new List<string> { s, s })
+            // remove the list with a cumulative result
+            .Fold("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+
+    [Fact(Skip = "skip")]
+    public void immutable_list()
+    {
+        // given an immutable list of ints (LangExt)
+        var result = Prelude.List(12, 9, 555)
+            // convert every value into a string (LangExt)
+            // keep the list with the same size
+            .Map(n => n.ToString())
+            // duplicate every value (LangExt)
+            // keep the list but change the size
+            .Bind(s => Prelude.List(s, s))
+            // remove the list with a cumulative result (LangExt)
+            .Fold("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+
+    [Fact(Skip = "skip")]
+    public void query_expression_list()
+    {
+        var result =
+            // create a list of ints
+            (from n in new List<int> { 12, 9, 555 }
+                // convert every value into a string
+                // keep the list with the same size
+                let s = n.ToString()
+                // duplicate every value
+                // keep the list but change the size
+                from doubled in new List<string> { s, s }
+                select doubled)
+            // remove the list with a cumulative result
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+
+    [Fact(Skip = "skip")]
+    public void query_expression_immutable_list()
+    {
+        var result =
+            // create a list of ints
+            (from n in Prelude.List(12, 9, 555)
+                // convert every value into a string
+                // keep the list with the same size
+                let s = n.ToString()
+                // duplicate every value
+                // keep the list but change the size
+                from doubled in Prelude.List(s, s)
+                select doubled)
+            // remove the list with a cumulative result
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        // TODO: complete the assert
+        // Assert.Equal(   , result);
+    }
+}
---
+++ IntroFp/Solutions/ListTests.cs
@@ -0,0 +1,79 @@
+using LanguageExt;
+using Xunit;
+
+namespace IntroFp.Solutions;
+
+public class ListTests
+{
+    [Fact]
+    public void linq_filled_list()
+    {
+        var result = new List<int> { 12, 9, 555 }
+            .Select(n => n.ToString())
+            .SelectMany(s => new List<string> { s, s })
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        Assert.Equal("12 12 9 9 555 555 ", result);
+    }
+
+    [Fact]
+    public void linq_empty_list()
+    {
+        var result = new List<int>()
+            .Select(n => n.ToString())
+            .SelectMany(s => new List<string> { s, s })
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void not_linq_but_functional_programming()
+    {
+        var result = new List<int> { 12, 9, 555 }
+            .Map(n => n.ToString())
+            .Bind(s => new List<string> { s, s })
+            .Fold("", (acc, cur) => acc + cur + " ");
+
+        Assert.Equal("12 12 9 9 555 555 ", result);
+    }
+
+    [Fact]
+    public void immutable_list()
+    {
+        var result = Prelude.List(12, 9, 555)
+            .Map(n => n.ToString())
+            .Bind(s => Prelude.List(s, s))
+            .Fold("", (acc, cur) => acc + cur + " ");
+
+        Assert.Equal("12 12 9 9 555 555 ", result);
+    }
+
+    [Fact]
+    public void query_expression_list()
+    {
+        var result =
+            (from n in new List<int> { 12, 9, 555 }
+                let s = n.ToString()
+                from doubled in new List<string> { s, s }
+                select doubled)
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+
+        Assert.Equal("12 12 9 9 555 555 ", result);
+    }
+
+    [Fact]
+    public void query_expression_immutable_list()
+    {
+        var result =
+            (from n in Prelude.List(12, 9, 555)
+                let s = n.ToString()
+                from doubled in Prelude.List(s, s)
+                select doubled)
+            .Aggregate("", (acc, cur) => acc + cur + " ");
+
+
+        Assert.Equal("12 12 9 9 555 555 ", result);
+    }
+}
